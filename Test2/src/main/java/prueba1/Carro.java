436d1c2f28470cde3376bd8433e064b58a32222c
Carlosdianzo15 <72307257+Carlosdianzo15@users.noreply.github.com>
2020-10-24 08:55:38 -0500

    testdianzo

---
+++ Test2/src/main/java/prueba1/Carro.java
@@ -0,0 +1,39 @@
+package prueba1;
+
+import com.sun.org.apache.xpath.internal.operations.Equals;
+import com.sun.tools.javac.launcher.Main;
+
+public class Carro {
+	static int numero;
+	
+	
+
+	public static void main(String[] args) {
+		numero=3;
+		int numero2=5;
+		
+		boolean verdaderoofalso;
+		
+		System.out.println(numero+numero2);
+		String cadenadecaracteres;
+		
+		cadenadecaracteres="Carlos Dianzo";
+	    System.out.println(cadenadecaracteres+" Eres el mejor");
+	    
+	   // verdaderoofalso = cadenadecaracteres.equals("carlos dianzo");
+	    
+	    
+	  
+	
+		
+		
+		
+	}
+	
+	
+	
+	
+	
+	
+
+}
---
+++ Test2/src/main/java/prueba1/prueba.java
@@ -0,0 +1,13 @@
+package prueba1;
+
+public class prueba {
+
+	public static void main(String[] args) {
+		
+		System.out.println("hola");
+		
+	}
+	
+	
+	
+}
